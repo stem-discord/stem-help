d4cd2b529da0d6c0c2a475c5578b6a81f9f60abb
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-21 20:52:28 +0900

    fix readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,36 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    mocha: true,
+    es6: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [`plugin:vue/vue3-essential`, `eslint:recommended`, `@vue/prettier`],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: `babel-eslint`,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": process.env.NODE_ENV === `production` ? `warn` : `off`,
+    "no-debugger": process.env.NODE_ENV === `production` ? `warn` : `off`,
+    "func-names": `off`,
+    "no-underscore-dangle": `off`,
+    "consistent-return": `off`,
+    "jest/expect-expect": `off`,
+    "security/detect-object-injection": `off`,
+
+    "linebreak-style": [`error`, `unix`],
+    semi: [`error`, `always`],
+    quotes: [`error`, `backtick`],
+    indent: [`error`, 2],
+    "quote-props": [`error`, `as-needed`],
+    strict: 0,
+    "comma-dangle": [`error`, `always-multiline`],
+    "space-infix-ops": [`error`, { int32Hint: false }],
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        `**/__tests__/*.{j,t}s?(x)`,
+        `**/tests/unit/**/*.spec.{j,t}s?(x)`,
       ],
       env: {
         mocha: true,
--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,18 @@ import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: `/`,
+    name: `Home`,
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: `/about`,
+    name: `About`,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ `../views/About.vue`),
   },
 ];
 
