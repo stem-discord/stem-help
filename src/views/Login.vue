940b5c869fd9f75aef44850183b47ffc9c8703f4
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-23 20:13:16 +0900

    template pages

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Login using Discord</h1>
+  <button @click="discordLogin">discord login</button>
+</template>
+
+<script setup>
+function discordLogin() {
+  // show new page in uri
+}
+</script>
---
+++ src/views/potd/MainPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>potd main page</h1>
+</template>
