3cce4c92165397b0240b324b65540c9499ec0422
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-23 20:12:11 +0900

    include current popup

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    popup: null,
+  },
+  mutations: {
+    setPopup(state, popup) {
+      state.popup = popup;
+    },
+  },
+  actions: {
+    setPopup({ commit }, popup) {
+      commit(`setPopup`, popup);
+    },
+  },
   modules: {},
 });
