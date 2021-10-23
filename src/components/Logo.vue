7f61521acc8ec242dbf9a996c2d0a3ad02fbb013
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-23 20:11:56 +0900

    text logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logo">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: `LOGO`,
+  },
+});
+</script>
+
+<style lang="scss">
+.logo {
+  p {
+    color: var(--main-color);
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    // enable anti aliasing
+    -webkit-font-smoothing: antialiased;
+    font-smooth: always;
+  }
+}
+</style>
