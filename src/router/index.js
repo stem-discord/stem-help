716c0355d3a8d8b9747f68f7527f48bcca1ac767
nopeless <38830903+nopeless@users.noreply.github.com>
2021-10-23 20:12:52 +0900

    add potd urls

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,21 @@ const routes = [
     component: Home,
   },
   {
+    path: `/login`,
+    name: `login page`,
+    component: () => import(`../views/Login.vue`),
+  },
+  {
+    path: `/potd`,
+    name: `potd main page`,
+    component: () => import(`../views/potd/MainPage.vue`),
+  },
+  {
+    path: `/potd/submit`,
+    name: `potd submit page`,
+    component: () => import(`../views/potd/Submit.vue`),
+  },
+  {
     path: `/about`,
     name: `About`,
     // route level code-splitting
@@ -18,9 +33,23 @@ const routes = [
   },
 ];
 
+// https://router.vuejs.org/guide/advanced/scroll-behavior.html
+function scrollBehavior(to, from, savedPosition) {
+  if (savedPosition && to.hash !== from.hash) {
+    return savedPosition;
+  }
+  if (to.hash) {
+    const item = document.querySelector(`a.anchor[href="${to.hash}"]`);
+    if (item) {
+      item.scrollIntoView({ behavior: `smooth` });
+    }
+  }
+}
+
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior,
 });
 
 export default router;
