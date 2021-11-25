2f21ecc92ba084a0796c910d7bfeddf664fb4f0f
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-25 21:05:49 +0900

    leaderboards

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ `../views/About.vue`),
   },
+  {
+    path: `/leaderboards`,
+    name: `Leaderboards`,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ `../views/leaderboards/Index.vue`),
+  },
 ];
 
 // https://router.vuejs.org/guide/advanced/scroll-behavior.html
