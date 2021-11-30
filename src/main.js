5bc7f2aa4e6a677b019d5e51f14c77e29280de10
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-30 15:05:03 +0900

    use VuePlugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { VuePlugin } from "vuera";
+
 import router from "./router";
 import store from "./store";
 import "./style.scss";
 
-createApp(App).use(store).use(router).mount(`#app`);
+createApp(App).use(store).use(router).use(VuePlugin).mount(`#app`);
