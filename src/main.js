b7bf306ad25af73046cb0394160f03253b4199ed
nopeless <38830903+nopeless@users.noreply.github.com>
2021-11-30 16:08:25 +0900

    use vuera

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { VuePlugin } from "vuera";
+import { VuePlugin } from "vuera-temporary";
 
 import router from "./router";
 import store from "./store";
 import "./style.scss";
 
-createApp(App).use(store).use(router).use(VuePlugin).mount(`#app`);
+createApp(App).use(VuePlugin).use(store).use(router).mount(`#app`);
