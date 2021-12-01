ce6994a6fc3eed011e72bf028eb7217420c28260
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-01 21:14:08 +0900

    even more fancy # resolver

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 // vue.config.js
 
+const path = require(`path`);
+
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
-module.exports = {};
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "#": path.resolve(__dirname, `src/components`),
+      },
+    },
+  },
+};
