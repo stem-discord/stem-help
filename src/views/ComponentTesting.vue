2ab69a4c0c9abfe7c83a399f86b4a7a5014fbf60
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-02 10:30:47 +0900

    add button component

--- src/views/ComponentTesting.vue
+++ src/views/ComponentTesting.vue
@@ -1,5 +1,28 @@
 <template>
-  <span>Button</span>
+  <div>
+    <h1>Component Testing</h1>
+    <div class="m-10">
+      <span>Button</span>
+      <Button>
+        <p style="font-size: 8px">NEXT</p>
+      </Button>
+      <br />
+      <Button>
+        <p style="font-size: 12px">NEXT</p>
+      </Button>
+      <br />
+      <Button>
+        <p style="font-size: 24px">NEXT</p>
+      </Button>
+      <br />
+      <Button>
+        <p style="font-size: 76px">NEXT</p>
+      </Button>
+      <button>
+        <p style="font-size: 40px">NEXT</p>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup>
