4dd26d401b6d6e355ad0ef1bb5cb8fb3e2a0db45
nopeless <nopeless@users.noreply.github.com>
2021-12-04 19:07:20 +0900

    add configuration testing page

---
+++ src/views/ComponentIntegrationTesting.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pl-5">
+    <h1>Component integration Testing</h1>
+    <div class="view">
+      <h2>Buttons</h2>
+      <Button @click="() => scriptClick(`Hello top`)">
+        <p style="font-size: 24px">NEXT</p>
+      </Button>
+      <Button @click="() => scriptClick(`Hello bottom`)">
+        <p style="font-size: 24px">NEXT</p>
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Button from "#/Button.vue";
+
+function scriptClick(msg) {
+  console.log(`Has clicked ${msg}`);
+}
+</script>
+
+<style lang="scss" scoped>
+.view {
+  @apply m-5;
+}
+</style>
