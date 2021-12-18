f07da8eed4340943dc3f18eee71d947c21f58eae
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-19 02:57:25 +0900

    remove vuera

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { VuePlugin } from "vuera-temporary";
 
 import router from "./router";
 import store from "./store";
 import "./style.scss";
 
-createApp(App).use(VuePlugin).use(store).use(router).mount(`#app`);
+createApp(App).use(store).use(router).mount(`#app`);
