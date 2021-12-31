85a5249b9c0ef7cf2dc4f3508d510501509a53df
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-31 19:28:17 +0900

    lets hope this fixes the font issue (3)

--- src/main.js
+++ src/main.js
@@ -5,4 +5,7 @@ import router from "./router";
 import store from "./store";
 import "./style.scss";
 
+import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
 createApp(App).use(store).use(router).mount(`#app`);
