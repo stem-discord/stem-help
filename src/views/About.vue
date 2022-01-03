facd04fc25dc68005cfdba92e12607bcf4770f3a
nopeless <38830903+nopeless@users.noreply.github.com>
2022-01-04 04:39:39 +0900

    make cringy about page. lets hope that I revise this soon owo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,77 @@
 <template>
-  <h1>STEM Helpers &amp; Students</h1>
-  <p>We strive to become a learning community for all</p>
+  <h1>STEM Homework Helpers &amp; Students</h1>
+
+  <h2>
+    Education is about <span class="underline">teaching</span> and
+    <span class="underline">learning</span>
+  </h2>
+
+  <div class="h-5"></div>
+
+  <p>
+    Monetization makes STEM learning a task. We believe in the philosophy of
+    free discourse and engagement
+  </p>
+
+  <p>A paywall will only limit those who need help the most</p>
+
+  <div class="h-10"></div>
+
+  <h2>Khan academy isn't fun</h2>
+
+  <p>Let's be real, when was the last time you posted memes in Khan academy?</p>
+  <p>
+    Khan academy is a great learning center, not for socialization and having a
+    place to talk
+  </p>
+
+  <div class="h-10"></div>
+
+  <h2>Smaller events, finding talents</h2>
+
+  <h3>
+    As organizations grow, they forget one key aspect of their identity:
+    tight-knit community
+  </h3>
+
+  <p>
+    Our server has definitely gone through that phase. Yet, we continue our
+    pursuit to offer room for smaller groups.
+  </p>
+
+  <div class="h-10"></div>
+
+  <h1>In some aspects, our Discord is maintained like a university</h1>
+
+  <p>
+    While abiding the strict rules, we also encourage others to hold it as their
+    own standards. We have a ban appeal server for we believed in reformed
+    members. Our server is as public as possible and offers great transparency,
+    by storing most of our information in GitHub since 2019
+    <a href="https://git.stem.help/public">https://git.stem.help/public</a>. All
+    of these workings reflect our philosophy in education
+  </p>
+
+  <h1>We value feedback</h1>
+
+  <p>
+    <a href="https://disboard.org/server/493173110799859713"
+      >Reviews on Disboard</a
+    >, suggestions in #issues-and-suggestions, issues in GitHub and pull
+    requests created by members are always reviewed. We recently implemented a
+    Kanban system in <a href="https://issues.stem.help">issues.stem.help</a> in
+    2021, and hope to make it partially public soon
+  </p>
 </template>
+
+<style lang="scss" scoped>
+p {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  font-size: 1.4rem;
+}
+
+* {
+  @apply select-text;
+}
+</style>
