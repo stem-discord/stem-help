7df683602f0856e28110496e1efbdf8ab1fddf36
nopeless <38830903+nopeless@users.noreply.github.com>
2022-01-04 04:42:45 +0900

    add some basic information and an iframe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="home">
     <h1>welcome to the STEM Helpers &amp; Students Discord website</h1>
-    <p>It's time to ditch Chegg</p>
+    <h4>It's time to ditch Chegg</h4>
 
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <p>A Server dedicated to helping with Homework and Discussions.</p>
+    <p>
+      SCIENCE, TECHNOLOGY, ENGINEERING & MATHEMATICS (STEM) Server for Students,
+      Professionals and Enthusiasts
+    </p>
+    <p>
+      Whether you're a student or just interested in learning about any of these
+      subjects or related fields, this server is for you!
+    </p>
+
+    <div class="h-10"></div>
+
+    <h1>Join the server</h1>
+
+    <p>
+      <a href="https://discord.gg/stem"> discord.gg/stem</a>
+
+      <iframe
+        src="https://discord.com/widget?id=493173110799859713&theme=dark"
+        width="350"
+        height="500"
+        allowtransparency="true"
+        frameborder="0"
+        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+      ></iframe>
+
+      <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+      <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+p {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+</style>
 
 <script>
 // @ is an alias to /src
