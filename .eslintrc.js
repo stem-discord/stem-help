7724a42069018b9f06667e4f4cc170e5deb1cf8d
nopeless <38830903+nopeless@users.noreply.github.com>
2022-01-04 05:07:03 +0900

    fix stupid prop error

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,13 @@ module.exports = {
     semi: [`error`, `always`],
     quotes: [`error`, `backtick`],
     indent: [`error`, 2],
-    "quote-props": [`error`, `as-needed`],
+    "quote-props": [
+      `error`,
+      `as-needed`,
+      {
+        numbers: true,
+      },
+    ],
     strict: 0,
     "comma-dangle": [`error`, `always-multiline`],
     "space-infix-ops": [`error`, { int32Hint: false }],
