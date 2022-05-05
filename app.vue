cb39defbed7984a699e1509e202919918a45a48d
nopeless <38830903+nopeless@users.noreply.github.com>
2022-05-06 01:53:21 +0900

    add basic login

--- app.vue
+++ app.vue
@@ -1,6 +1,34 @@
 <template>
+  <div class="flex">
+    <h1 class="mr-0 w-fit">STOOM SERVER OWO</h1>
+    <div class="p-4 w-fit ml-auto">
+      <div style="right: 0; width: 500px" class="">
+        <v-form>
+          <div class="flex">
+            <v-text-field
+              v-model="username"
+              label="username"
+              density="compact"
+              hide-details="true"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="password"
+              density="compact"
+              hide-details="true"
+            ></v-text-field>
+            <v-btn class="h-full self-center" @click="login">Login</v-btn>
+          </div>
+        </v-form>
+      </div>
+    </div>
+  </div>
   <NuxtPage />
 </template>
+
+<script setup>
+function login() {}
+</script>
 
 <style>
 @import '~/assets/css/tailwind.css';
